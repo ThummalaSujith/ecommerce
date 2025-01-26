3ed2aae0b5dcaf0df40edb37eb239d18f8de2a99
Thummala Sujith <srt7@njit.edu>
2025-01-26 17:17:35 -0500

    created authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,9 +2,9 @@ import User from "../models/userModel.js";
 
 import asyncHandler from "../middlewares/asyncHandler.js";
 
-import bcrypt from "bcryptjs/dist/bcrypt.js"
+import bcrypt from "bcryptjs/dist/bcrypt.js";
 
-import createToken from "../utils/createToken.js"
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
@@ -19,58 +19,66 @@ const createUser = asyncHandler(async (req, res) => {
   const userExists = await User.findOne({ email });
   if (userExists) res.status(400).send("User already exists");
 
+  const salt = await bcrypt.genSalt(10);
 
-  const salt = await bcrypt.genSalt(10)
+  const hashedpassword = await bcrypt.hash(password, salt);
 
-  const hashedpassword = await bcrypt.hash(password,salt)
-
-  const newUser = new User({ username, email, password:hashedpassword });
+  const newUser = new User({ username, email, password: hashedpassword });
 
   try {
     await newUser.save();
-    createToken(res,newUser._id)
-    res
-      .status(201)
-      .json({
-        _id: newUser._id,
-        username: newUser.username,
-        email: newUser.email,
-        isAdmin: newUser.isAdmin,
-      });
+    createToken(res, newUser._id);
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
   } catch (error) {
     res.status(400);
     throw new Error("Invalid User data");
   }
 });
 
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-const loginUser = asyncHandler(async(req,res)=>{
+  const existingUser = await User.findOne({ email });
 
-const {email , password}=req.body
+  if (existingUser) {
+    const ispasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
 
-const  existingUser = await User.findOne({email})
+    if (ispasswordValid) {
+      createToken(res, existingUser._id);
 
+      res.status(201).json({
+        _id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+        isAdmin: existingUser.isAdmin,
+      });
 
-if(existingUser){
-    const ispasswordValid = await bcrypt.compare(password,existingUser.password)
-
-    if(ispasswordValid ){
-        createToken(res,existingUser._id)
-
-        res.status(201).json({
-            _id:existingUser._id,
-            username:existingUser.username,
-            email:existingUser.email,
-            isAdmin:existingUser.isAdmin
-        })
-
-        return  //exit the function after sending the response
+      return; //exit the function after sending the response
     }
-}
+  }
+});
 
 
 
-
+const logoutCurrentUser = asyncHandler(async(req,res)=>{
+    res.cookie("jwt", "" ,{
+        httpOnly:true,
+        expires:new Date(0)
+    })
+    res.status(200).json({message:"Logged out successfully"})
 })
 
-export { createUser ,loginUser };
+const getAllUsers = asyncHandler(async(req,res)=>{
+    const users= await User.find({})
+    res.json(users)
+})
+
+export { createUser, loginUser ,logoutCurrentUser , getAllUsers};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express"
-import { createUser,loginUser } from "../controllers/userController.js";
+import { createUser,loginUser , logoutCurrentUser, getAllUsers} from "../controllers/userController.js";
+
+
+import { authenticate,authorizeAdmin } from "../middlewares/authmiddleware.js";
 
 const router = express.Router()
 
-router.route("/").post(createUser)
+router.route("/").post(createUser).get(authenticate,authorizeAdmin,getAllUsers)
 
 router.post("/auth",loginUser)
 
