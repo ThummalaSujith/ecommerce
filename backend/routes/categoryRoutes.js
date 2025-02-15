cd550ff8520ffafd445766148f73eb9562a4b2f5
Thummala Sujith <srt7@njit.edu>
2025-02-15 15:44:34 -0500

    created route to update Category as an admin

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createCategory } from "../controllers/categoryController.js";
+import { createCategory,updateCategory } from "../controllers/categoryController.js";
 
 const router = express.Router()
 
@@ -7,6 +7,10 @@ const router = express.Router()
 import { authenticate,authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 
-router.route('/').post(createCategory)
+router.route('/').post(authenticate,authorizeAdmin,createCategory)
+
+router.route("/:categoryId").put(authenticate,authorizeAdmin,updateCategory)
+
+router.route("/:categoryId").delete(authenticate,authorizeAdmin,deleteCategory)
 
 export default router;
