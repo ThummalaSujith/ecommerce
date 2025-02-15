bb608630bec150625b63a1539b481f09c4564b68
Thummala Sujith <srt7@njit.edu>
2025-02-15 15:45:53 -0500

    created categoryController to update category

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,6 +1,7 @@
 import Category from "../models/categoryModel.js";
 
 import asyncHandler from "../middlewares/asyncHandler.js";
+import internal from "stream";
 
 const createCategory = asyncHandler(async (req, res) => {
   try {
@@ -19,7 +20,7 @@ const createCategory = asyncHandler(async (req, res) => {
 
     const category = await new Category({ name }).save();
 
-    res.json(category)
+    res.json(category);
 
     console.log(name);
   } catch (error) {
@@ -28,4 +29,27 @@ const createCategory = asyncHandler(async (req, res) => {
   }
 });
 
-export { createCategory };
+const updateCategory = asyncHandler(async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    const { categoryId } = req.params;
+
+    const category = await Category.findOne({ _id: categoryId });
+
+    if (!category) {
+      return res.status(404).json({ error: "Category not Found" });
+    }
+
+    category.name = name;
+
+    const updateCategory = await category.save();
+
+    res.json(updateCategory);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+export { createCategory, updateCategory };
