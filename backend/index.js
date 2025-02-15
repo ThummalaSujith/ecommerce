e1bd41538b0ab332ec72b2a919f9d1001c457968
Thummala Sujith <srt7@njit.edu>
2025-02-15 12:18:33 -0500

    Add routing for category API endpoints

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ import cors from "cors"; // Import the cors package
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 
+import categoryRoutes from "./routes/categoryRoutes.js"
+
 dotenv.config();
 const port = process.env.PORT || 5000;
 
@@ -29,6 +31,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
+
+app.use("/api/category",categoryRoutes)
 
 app.listen(port, () => console.log(`Server running on port : ${port}`));
 
